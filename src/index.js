4aa79ed542225446f4548f008c690a73d7299ecc
englishenotua <115879869+englishenotua@users.noreply.github.com>
2022-11-12 21:05:42 +0200

    dfgh
    fdgh

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './main.css'
+
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
+import { createStore, applyMiddleware } from 'redux'
+import { createBrowserHistory } from 'history'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { ConnectedRouter, routerMiddleware } from 'connected-react-router'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+import createRootReducer from 'reducers'
+import Layout from 'containers/layout'
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+
+root.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Layout />
+        </ConnectedRouter>
+    </Provider>
+)
